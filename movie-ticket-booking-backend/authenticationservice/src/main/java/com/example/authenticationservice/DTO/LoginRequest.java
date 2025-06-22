5c4b66437ca3e1a6fc90a0a7b7b71f0a6beda12a
Niro-dev91 <npothupitiya.gitlab@gmail.com>
2025-06-22 13:22:33 +0530

    file changed

--- movie-ticket-booking-backend/authenticationservice/src/main/java/com/example/authenticationservice/DTO/LoginRequest.java
+++ movie-ticket-booking-backend/authenticationservice/src/main/java/com/example/authenticationservice/DTO/LoginRequest.java
@@ -1,11 +1,11 @@
 package com.example.authenticationservice.DTO;
 
-public class LoginRequest {
+public class LoginRequest { 
     private String username;
     private String password;
 
     
-    public String getUsername() { return username; }
+    public String getUsername() { return username; } //username
     public void setUsername(String username) { this.username = username; }
 
     public String getPassword() { return password; }
