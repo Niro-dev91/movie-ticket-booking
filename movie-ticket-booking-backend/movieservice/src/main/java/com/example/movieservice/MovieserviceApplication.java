755997b4e2116cc0099fdaa3cac6c9c4a6f6c969
Niro-dev91 <npothupitiya.gitlab@gmail.com>
2026-06-10 14:24:10 +0530

    Implemented OpenFeign client in movie-service to fetch user details from authentication-service by user ID. Added user lookup endpoint in authentication-service and integrated it with payment flow to retrieve customer details between services.

--- movie-ticket-booking-backend/movieservice/src/main/java/com/example/movieservice/MovieserviceApplication.java
+++ movie-ticket-booking-backend/movieservice/src/main/java/com/example/movieservice/MovieserviceApplication.java
@@ -3,9 +3,11 @@ package com.example.movieservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableFeignClients
 public class MovieserviceApplication {
 
 	public static void main(String[] args) {
